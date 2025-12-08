5733dbdd91be6e2bda88ef994290641a7e7e0732
Matan Tenenbaum <matanten@gmail.com>
2025-12-08 20:04:05 +0200

    prepare for deployment on AWS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,11 +29,9 @@ app.get("/health", (req, res) => {
     res.json({ health: "Success" });
 });
 
-
-
 mongoose.connection.once('open', () => {
     console.log("Connected to MongoDB")
-    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
-
