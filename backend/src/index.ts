8dd3268a8e1296691809f1df5448a2752740d682
Matan Tenenbaum <matanten@gmail.com>
2025-12-08 21:52:02 +0200

    cicd and cors changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173', // Your frontend URL
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials: true // Allow cookies to be sent
 }))
 app.use(cookieParser())
