31c133123ae70468a90179158e9869f35f37e2c2
Matan Tenenbaum <matanten@gmail.com>
2026-06-06 22:27:53 +0300

    Update dashboardController.ts

--- backend/src/controller/trainer/dashboardController.ts
+++ backend/src/controller/trainer/dashboardController.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express";
 import TrainerClientRelation from "../../models/TrainerClientRelation.js";
-import User from "../../models/user/User";
-import Session from "../../models/Session";
+import User from "../../models/user/User.js";
+import Session from "../../models/Session.js";
 import { clerkClient } from "@clerk/express";
 
 
